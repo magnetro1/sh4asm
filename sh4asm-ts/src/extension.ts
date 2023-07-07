4e3d3a14128ac1088188cb8320594a86df468192
magnetro1 <60054111+magnetro1@users.noreply.github.com>
2023-07-07 02:47:52 -0700

    added codeblock back to hover

--- sh4asm-ts/src/extension.ts
+++ sh4asm-ts/src/extension.ts
@@ -2,12 +2,12 @@ import * as vscode from 'vscode';
 import * as staticData from './sh4asm_staticData';
 import * as path from 'path';
 
-const localPath = 'supportMedia/characterThumbnails'
+const thumbsPath = 'supportMedia/characterThumbnails'
 
 export function activate(context: vscode.ExtensionContext) {
 
   let disposable = vscode.commands.registerCommand('sh4asm.helloWorld', () => {
-    vscode.window.showInformationMessage('Hello World from sh4asm-ts! This is hard');
+    vscode.window.showInformationMessage('Hello World from sh4asm-ts!');
   });
 
   let box = vscode.commands.registerCommand('sh4asm.box', function () {
@@ -34,35 +34,49 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(disposable);
   context.subscriptions.push(box);
 
-
+  // Hover character definition
   vscode.languages.registerHoverProvider('sh4asm', {
     provideHover(document, position, token) {
       const range = document.getWordRangeAtPosition(position,
         /0x\d+\w+(?=.*char)|0x\d+\d+(?=.*char)/);
       const word = document.getText(range);
+      // Hex math for character assist values
       let hexPrefix = '0x';
       let baseVal = parseInt(word, 16);
-      let assistA: number | string = 0;
+      // let assistA: number | string = 0; // doesn't need to be calculated
       let assistB: number | string = 0;
       let assistC: number | string = 0;
-      assistA = baseVal.toString(16);
+      // assistA = baseVal.toString(16);
       // assistA = assistA.toString(16);
       assistB = baseVal + 64;
       assistB = assistB.toString(16);
       assistC = baseVal + 128;
       assistC = assistC.toString(16);
+      // Create hover for character image
       let imageMD = new vscode.MarkdownString();
+      imageMD.baseUri = vscode.Uri.file(path.join(context.extensionPath, thumbsPath, path.sep));
+      let imageHover = imageMD.appendMarkdown(`![char](${imageMD.baseUri}/charID_${word}.jpg)`);
+
+      // let dataMD = new vscode.MarkdownString();
+      // dataMD.appendMarkdown(
+      //   `\n\n\*Name:\* ${staticData.characters_Hex_2_Names[word]}`
+      //   + `\n\n\*Hex:\* ${word}`
+      //   + `\n\n\*Decimal:\* ${parseInt(word, 16)}`
+      //   + `\n\n\*Assist - α:\* ${word}` // doesn't need to be calculated
+      //   + `\n\n\*Assist - β:\* ${hexPrefix + assistB}`
+      //   + `\n\n\*Assist - γ:\* ${hexPrefix + assistC}`
+      // );
       let dataMD = new vscode.MarkdownString();
-      imageMD.baseUri = vscode.Uri.file(path.join(context.extensionPath, localPath, path.sep));
-      const imageHover = imageMD.appendMarkdown(`![char](${imageMD.baseUri}/charID_${word}.jpg)`);
-      dataMD.appendMarkdown(
-        `\n\n\*Name:\* ${staticData.characters_Hex_2_Names[word]}`
-        + `\n\n\*Hex:\* ${word}`
-        + `\n\n\*Decimal:\* ${parseInt(word, 16)}`
-        + `\n\n\*Assist - α:\* ${word} `
-        + `\n\n\*Assist - β:\* ${hexPrefix + assistB}`
-        + `\n\n\*Assist - γ:\* ${hexPrefix + assistC}`
-      );
+      // Format the "Name" in bold
+      dataMD.appendMarkdown(`\n\nName: ${staticData.characters_Hex_2_Names[word]}\n\n`);
+      dataMD.appendCodeblock(`Hex: ${word}`, 'sh4asm');
+      dataMD.appendCodeblock(`Decimal: ${parseInt(word, 16)}`, 'sh4asm');
+
+      dataMD.appendCodeblock(`Assist - α: ${word}`, 'sh4asm');
+      dataMD.appendCodeblock(`Assist - β: ${hexPrefix + assistB}`, 'sh4asm');
+      dataMD.appendCodeblock(`Assist - γ: ${hexPrefix + assistC}`, 'sh4asm');
+
+      // dataMD.appendMarkdown(`\n\n\*Name:\* ${staticData.characters_Hex_2_Names[word]}`);
       const dataHover = dataMD;
       if (Object.keys(staticData.characters_Hex_2_Names).includes(word)) {
         return new vscode.Hover(
@@ -71,13 +85,6 @@ export function activate(context: vscode.ExtensionContext) {
       }
     }
   });
-  // {
-  //   provideHover() {
-  //     const markdownString = new vscode.MarkdownString();
-  // markdownString.appendText("Hello: https://github.com/microsoft/vscode/issues/142494")
-  //     return new vscode.Hover(markdownString);
-  //   }
-  // })
 
   // Hover MOV.L definition
   // vscode.languages.registerHoverProvider('sh4asm', {
